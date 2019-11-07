c255b3eba4e91b20d1125a269152e750f6b69e3f
傅志强 <648706161@qq.com>
2019-11-07 13:01:15 +0800

    将java工程添加到版本库

---
+++ myProject1/src/MyProject.java
@@ -0,0 +1,6 @@
+public class MyProject {
+    public static void main(String[] args) {
+        System.out.println("111");
+        Test1 test1 = new Test1();
+    }
+}
---
+++ myProject1/src/Test1.java
@@ -0,0 +1,9 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
+public class Test1 {
+
+    private  String name;
+
+}
